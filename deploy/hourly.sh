f594b33c0c7b8011518b1780e12ef169c6111a03
James O'Toole <jtotoole@gmail.com>
2019-03-19 18:40:21 -0400

    fix agency .envs in deploy scripts

--- deploy/hourly.sh
+++ deploy/hourly.sh
@@ -3,110 +3,34 @@
 # Government Wide
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
 
-# Department of Education
-source $ANALYTICS_ROOT_PATH/deploy/envs/education.env
+# Burgh's Eye View
+source $ANALYTICS_ROOT_PATH/deploy/envs/burghs-eye-view.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
 
-# National Aeronautics and Space Administration
-source $ANALYTICS_ROOT_PATH/deploy/envs/national-aeronautics-space-administration.env
+# Fire Recruitment
+source $ANALYTICS_ROOT_PATH/deploy/envs/fire-recruitment.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
 
-# Department of Justice
-source $ANALYTICS_ROOT_PATH/deploy/envs/justice.env
+# My Brother's Keeper
+source $ANALYTICS_ROOT_PATH/deploy/envs/my-brothers-keeper.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
 
-# Department of Veterans Affairs
-source $ANALYTICS_ROOT_PATH/deploy/envs/veterans-affairs.env
+# OnePGH
+source $ANALYTICS_ROOT_PATH/deploy/envs/one-pgh.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
 
-# Department of Commerce
-source $ANALYTICS_ROOT_PATH/deploy/envs/commerce.env
+# OneStopPGH
+source $ANALYTICS_ROOT_PATH/deploy/envs/one-stop-pgh.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
 
-# Environmental Protection Agency
-source $ANALYTICS_ROOT_PATH/deploy/envs/environmental-protection-agency.env
+# Pittsburgh Parking Authority
+source $ANALYTICS_ROOT_PATH/deploy/envs/pittsburgh-parking-authority.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
 
-# Small Business Administration
-source $ANALYTICS_ROOT_PATH/deploy/envs/small-business-administration.env
+# Pittsburgh Purchasing
+source $ANALYTICS_ROOT_PATH/deploy/envs/pittsburgh-purchasing.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
 
-# Department of Energy
-source $ANALYTICS_ROOT_PATH/deploy/envs/energy.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
-
-# Department of the Interior
-source $ANALYTICS_ROOT_PATH/deploy/envs/interior.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
-
-# National Archives and Records Administration
-source $ANALYTICS_ROOT_PATH/deploy/envs/national-archives-records-administration.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
-
-# Department of Agriculture
-source $ANALYTICS_ROOT_PATH/deploy/envs/agriculture.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
-
-# Department of Defense
-source $ANALYTICS_ROOT_PATH/deploy/envs/defense.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
-
-# Department of Health and Human Services
-source $ANALYTICS_ROOT_PATH/deploy/envs/health-human-services.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
-
-# Department of Housing and Urban Development
-source $ANALYTICS_ROOT_PATH/deploy/envs/housing-urban-development.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
-
-# Department of Homeland Security
-source $ANALYTICS_ROOT_PATH/deploy/envs/homeland-security.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
-
-# Department of Labor
-source $ANALYTICS_ROOT_PATH/deploy/envs/labor.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
-
-# Department of State
-source $ANALYTICS_ROOT_PATH/deploy/envs/state.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
-
-# Department of Transportation
-source $ANALYTICS_ROOT_PATH/deploy/envs/transportation.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
-
-# Department of the Treasury
-source $ANALYTICS_ROOT_PATH/deploy/envs/treasury.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
-
-# Agency for International Development
-source $ANALYTICS_ROOT_PATH/deploy/envs/agency-international-development.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
-
-# General Services Administration
-source $ANALYTICS_ROOT_PATH/deploy/envs/general-services-administration.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
-
-# National Science Foundation
-source $ANALYTICS_ROOT_PATH/deploy/envs/national-science-foundation.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
-
-# Nuclear Regulatory Commission
-source $ANALYTICS_ROOT_PATH/deploy/envs/nuclear-regulatory-commission.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
-
-# Office of Personnel Management
-source $ANALYTICS_ROOT_PATH/deploy/envs/office-personnel-management.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
-
-# Social Security Administration
-source $ANALYTICS_ROOT_PATH/deploy/envs/social-security-administration.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
-
-# Postal Service
-source $ANALYTICS_ROOT_PATH/deploy/envs/postal-service.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
-
-# Executive Office of the President
-source $ANALYTICS_ROOT_PATH/deploy/envs/executive-office-president.env
+# Police Recruitment
+source $ANALYTICS_ROOT_PATH/deploy/envs/police-recruitment.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=hourly --slim --verbose
--- deploy/realtime.sh
+++ deploy/realtime.sh
@@ -5,137 +5,42 @@ $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbo
 # we want just one realtime report in CSV, hardcoded for now to save on API requests
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
 
-# Department of Education
-source $ANALYTICS_ROOT_PATH/deploy/envs/education.env
+# Burgh's Eye View
+source $ANALYTICS_ROOT_PATH/deploy/envs/burghs-eye-view.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
 
-# Department of Veterans Affairs
-source $ANALYTICS_ROOT_PATH/deploy/envs/veterans-affairs.env
+# Fire Recruitment
+source $ANALYTICS_ROOT_PATH/deploy/envs/fire-recruitment.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
 
-# National Aeronautics and Space Administration
-source $ANALYTICS_ROOT_PATH/deploy/envs/national-aeronautics-space-administration.env
+# My Brother's Keeper
+source $ANALYTICS_ROOT_PATH/deploy/envs/my-brothers-keeper.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
 
-# Department of Justice
-source $ANALYTICS_ROOT_PATH/deploy/envs/justice.env
+# OnePGH
+source $ANALYTICS_ROOT_PATH/deploy/envs/one-pgh.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
 
-# Department of Commerce
-source $ANALYTICS_ROOT_PATH/deploy/envs/commerce.env
+# OneStopPGH
+source $ANALYTICS_ROOT_PATH/deploy/envs/one-stop-pgh.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
 
-# Environmental Protection Agency
-source $ANALYTICS_ROOT_PATH/deploy/envs/environmental-protection-agency.env
+# Pittsburgh Parking Authority
+source $ANALYTICS_ROOT_PATH/deploy/envs/pittsburgh-parking-authority.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
 
-# Small Business Administration
-source $ANALYTICS_ROOT_PATH/deploy/envs/small-business-administration.env
+# Pittsburgh Purchasing
+source $ANALYTICS_ROOT_PATH/deploy/envs/pittsburgh-purchasing.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
 
-# Department of Energy
-source $ANALYTICS_ROOT_PATH/deploy/envs/energy.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
-
-# Department of the Interior
-source $ANALYTICS_ROOT_PATH/deploy/envs/interior.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
-
-# National Archives and Records Administration
-source $ANALYTICS_ROOT_PATH/deploy/envs/national-archives-records-administration.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
-
-# Department of Agriculture
-source $ANALYTICS_ROOT_PATH/deploy/envs/agriculture.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
-
-# Department of Defense
-source $ANALYTICS_ROOT_PATH/deploy/envs/defense.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
-
-# Department of Health and Human Services
-source $ANALYTICS_ROOT_PATH/deploy/envs/health-human-services.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
-
-# Department of Housing and Urban Development
-source $ANALYTICS_ROOT_PATH/deploy/envs/housing-urban-development.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
-
-# Department of Homeland Security
-source $ANALYTICS_ROOT_PATH/deploy/envs/homeland-security.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
-
-# Department of Labor
-source $ANALYTICS_ROOT_PATH/deploy/envs/labor.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
-
-# Department of State
-source $ANALYTICS_ROOT_PATH/deploy/envs/state.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
-
-# Department of Transportation
-source $ANALYTICS_ROOT_PATH/deploy/envs/transportation.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
-
-# Department of the Treasury
-source $ANALYTICS_ROOT_PATH/deploy/envs/treasury.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
-
-# Agency for International Development
-source $ANALYTICS_ROOT_PATH/deploy/envs/agency-international-development.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
-
-# General Services Administration
-source $ANALYTICS_ROOT_PATH/deploy/envs/general-services-administration.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
-
-# National Science Foundation
-source $ANALYTICS_ROOT_PATH/deploy/envs/national-science-foundation.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
-
-# Nuclear Regulatory Commission
-source $ANALYTICS_ROOT_PATH/deploy/envs/nuclear-regulatory-commission.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
-
-# Office of Personnel Management
-source $ANALYTICS_ROOT_PATH/deploy/envs/office-personnel-management.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
-
-# Social Security Administration
-source $ANALYTICS_ROOT_PATH/deploy/envs/social-security-administration.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
-
-# Postal Service
-source $ANALYTICS_ROOT_PATH/deploy/envs/postal-service.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
-
-# Executive Office of the President
-source $ANALYTICS_ROOT_PATH/deploy/envs/executive-office-president.env
+# Police Recruitment
+source $ANALYTICS_ROOT_PATH/deploy/envs/police-recruitment.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
