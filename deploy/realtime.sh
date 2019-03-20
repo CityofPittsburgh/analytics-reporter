faab4afc1e812118e04741984657f573cf0af568
James O'Toole <jtotoole@gmail.com>
2019-03-20 19:38:07 -0400

    remove all-pages-realtime.csv report from realtime.sh

--- deploy/realtime.sh
+++ deploy/realtime.sh
@@ -2,45 +2,35 @@
 
 # Government Wide
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-# we want just one realtime report in CSV, hardcoded for now to save on API requests
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
 
 # Burgh's Eye View
 source $ANALYTICS_ROOT_PATH/deploy/envs/burghs-eye-view.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
 
 # Fire Recruitment
 source $ANALYTICS_ROOT_PATH/deploy/envs/fire-recruitment.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
 
 # My Brother's Keeper
 source $ANALYTICS_ROOT_PATH/deploy/envs/my-brothers-keeper.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
 
 # OnePGH
 source $ANALYTICS_ROOT_PATH/deploy/envs/one-pgh.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
 
 # OneStopPGH
 source $ANALYTICS_ROOT_PATH/deploy/envs/one-stop-pgh.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
 
 # Pittsburgh Parking Authority
 source $ANALYTICS_ROOT_PATH/deploy/envs/pittsburgh-parking-authority.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
 
 # Pittsburgh Purchasing
 source $ANALYTICS_ROOT_PATH/deploy/envs/pittsburgh-purchasing.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
 
 # Police Recruitment
 source $ANALYTICS_ROOT_PATH/deploy/envs/police-recruitment.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=realtime --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --only=all-pages-realtime --slim --verbose --csv
