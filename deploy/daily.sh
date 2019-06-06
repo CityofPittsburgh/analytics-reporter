a8743544e1fbe87ffbfd60d87cc8bea783a15301
James O'Toole <jtotoole@gmail.com>
2019-06-06 19:41:14 -0400

    remove unused agencies

--- deploy/daily.sh
+++ deploy/daily.sh
@@ -14,28 +14,8 @@ source $ANALYTICS_ROOT_PATH/deploy/envs/fire-recruitment.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=daily --slim --verbose
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=daily --slim --verbose --csv
 
-# My Brother's Keeper
-source $ANALYTICS_ROOT_PATH/deploy/envs/my-brothers-keeper.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=daily --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=daily --slim --verbose --csv
-
-# OnePGH
-source $ANALYTICS_ROOT_PATH/deploy/envs/one-pgh.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=daily --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=daily --slim --verbose --csv
-
-# OneStopPGH
-source $ANALYTICS_ROOT_PATH/deploy/envs/one-stop-pgh.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=daily --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=daily --slim --verbose --csv
-
 # Pittsburgh Parking Authority
 source $ANALYTICS_ROOT_PATH/deploy/envs/pittsburgh-parking-authority.env
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=daily --slim --verbose
-$ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=daily --slim --verbose --csv
-
-# Pittsburgh Purchasing Authority
-source $ANALYTICS_ROOT_PATH/deploy/envs/pittsburgh-purchasing.env
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=daily --slim --verbose
 $ANALYTICS_ROOT_PATH/bin/analytics --publish --frequency=daily --slim --verbose --csv
 
